3a969a477654940fa3821755d7483ada0a784de1
MuffMuffin <ymakukha@gmail.com>
2022-12-11 16:28:22 +0200

    Reverting to the good stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,5 @@
-import { Profile } from './Profile/Profile';
-import user from '../data/user.json';
-import { Statistics } from './Statistics/Statistics';
-import data from '../data/data.json';
-import { FriendList } from './FriendList/FriendList';
-import friends from '../data/friends.json';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from '../data/transactions.json';
+import { Holder } from './Holder/Holder';
 
 export const App = () => {
-  return (
-    <>
-      <Profile user={user} />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
-  );
+  return <Holder />;
 };
---
+++ src/components/Holder/Holder.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+
+import { Profile } from '../Profile/Profile';
+import user from '../../data/user.json';
+import { Statistics } from '../Statistics/Statistics';
+import data from '../../data/data.json';
+import { FriendList } from '../FriendList/FriendList';
+import friends from '../../data/friends.json';
+import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
+import transactions from '../../data/transactions.json';
+import css from './Holder.module.css';
+
+export const Holder = () => {
+  const [activeTab, setActiveTab] = useState('tab1');
+  useEffect(() => {
+    document.title = 'GoIT React Homework 1';
+  }, []);
+  return (
+    <div className={css.holder}>
+      <ul className={css.selector}>
+        <li
+          className={activeTab === 'tab1' ? css.selected : css.item}
+          onClick={() => setActiveTab('tab1')}
+        >
+          Profile
+        </li>
+        <li
+          className={activeTab === 'tab2' ? css.selected : css.item}
+          onClick={() => setActiveTab('tab2')}
+        >
+          Statistics
+        </li>
+        <li
+          className={activeTab === 'tab3' ? css.selected : css.item}
+          onClick={() => setActiveTab('tab3')}
+        >
+          Friend List
+        </li>
+        <li
+          className={activeTab === 'tab4' ? css.selected : css.item}
+          onClick={() => setActiveTab('tab4')}
+        >
+          Transaction History
+        </li>
+      </ul>
+
+      <div className={css.output}>
+        {activeTab === 'tab1' ? <Profile user={user} /> : ''}
+        {activeTab === 'tab2' ? <Statistics stats={data} /> : ''}
+        {activeTab === 'tab3' ? <FriendList friends={friends} /> : ''}
+        {activeTab === 'tab4' ? (
+          <TransactionHistory items={transactions} />
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
+};
